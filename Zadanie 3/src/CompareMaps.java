b896d048ad065709c5d6a3a3c46c0f01455cbe7c
Hanna <hania.kuczynska@gmail.com>
2024-10-24 14:14:06 +0200

    Zrobione Zadanie 3

---
+++ Zadanie 3/src/CompareMaps.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CompareMaps {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+
+        hashMap.put("Amelia", 6);
+        hashMap.put("Ala", 7);
+        hashMap.put("Hania", 13);
+
+        treeMap.put("Amelia", 6);
+        treeMap.put("Ala", 7);
+        treeMap.put("Hania", 13);
+
+        System.out.println("HashMap - kolejność dowolna: " + hashMap);
+        System.out.println("TreeMap - kolejność posortowana według kluczy: " + treeMap);
+    }
+}

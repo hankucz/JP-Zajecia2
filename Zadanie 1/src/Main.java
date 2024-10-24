32a38c60ee7e9ceb6841255da18841f8fe764fd9
Hanna <hania.kuczynska@gmail.com>
2024-10-24 13:40:15 +0200

    Zrobione zadanie 1

---
+++ Zadanie 1/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashSet<Integer> hashSet = new HashSet<>();
+        TreeSet <Integer> treeSet = new TreeSet<>();
+
+        System.out.println("Podaj liczby, oddzielone spacjami: ");
+        String[] input = scanner.nextLine().split(" ");
+        for(String number : input) {
+            int value = Integer.parseInt(number);
+            hashSet.add(value);
+            treeSet.add(value);
+        }
+
+        System.out.println("HashSet - kolejność dowolna: " + hashSet);
+        System.out.println("TreeSet - kolejność posortowana rosnąco: " + treeSet);
+    }
+}
